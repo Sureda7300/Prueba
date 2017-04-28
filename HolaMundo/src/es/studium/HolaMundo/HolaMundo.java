9b24b4b0892210acb21201fc87c302d039bc3ac7
JFdez <JFdez@ASUS-PC>
2017-04-28 09:15:09 +0200

    Commit tras importar en otro equipo

--- HolaMundo/src/es/studium/HolaMundo/HolaMundo.java
+++ HolaMundo/src/es/studium/HolaMundo/HolaMundo.java
@@ -3,7 +3,8 @@ package es.studium.HolaMundo;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");  
+		System.out.println("Hola Mundo"); 
+		System.out.println("Nueva línea añadida desde otro Pc tras importar el repositorio");
 
 	}
 
